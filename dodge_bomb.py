11bd6b5061f870d0aff980e178e2ede1e69ea6f0
chrystyvera <c0a251360f@edu.teu.ac.jp>
2026-06-16 14:49:49 +0900

    練習2のコミットです。

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random 
 import pygame as pg
 
 
@@ -21,6 +22,16 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img= pg.Surface((20,20))
+    bb_img.set_colorkey((0,0,0))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_rct=bb_img.get_rect()
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery= random.randint(0, HEIGHT)
+    vx, vy = +5, +5
+
+    bb_img.set_colorkey((0,0,0))
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -39,6 +50,8 @@ def main():
 
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
