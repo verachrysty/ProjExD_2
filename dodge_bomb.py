4287c57215f1166cc04a859695e1fea342a0832b
chrystyvera <c0a251360f@edu.teu.ac.jp>
2026-06-16 14:59:36 +0900

    練習4のコミットです。

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -51,6 +51,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bb_rct): 
+            print("ゲームオーバー")
+            return
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
