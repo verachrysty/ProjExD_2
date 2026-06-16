ce7effd7724aa857e800f92a8debb348778e34f7
chrystyvera <c0a251360f@edu.teu.ac.jp>
2026-06-16 14:55:31 +0900

    練習3のコミットです。

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,6 +14,19 @@ DELTA = { # first exercice
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRect or 爆弾Rect
+    戻り値：判定結果タプル（横方向判定結果，縦方向判定結果）
+    True：画面内／False：画面外
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:  
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom: 
+        tate = False
+    return yoko, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -49,8 +62,15 @@ def main():
                 sum_mv[1] += mv[1]  
 
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) 
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko: 
+            vx *= -1
+        if not tate: 
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
