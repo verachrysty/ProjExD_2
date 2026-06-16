1398e57b03923fa99349c5640391414f8f8646b9
chrystyvera <c0a251360f@edu.teu.ac.jp>
2026-06-16 17:01:59 +0900

    追加機能2完成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -15,7 +15,7 @@ DELTA = { # first exercice
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-#演習１
+
 def gameover(screen: pg.surface) -> None:
     black_scr = pg.Surface((WIDTH, HEIGHT))
     black_scr.fill((0,0,0))
@@ -37,6 +37,19 @@ def gameover(screen: pg.surface) -> None:
     pg.display.update()
     time.sleep(5)
 
+    
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+       
+    bb_imgs = []
+    bb_accs = [a for a in range(1, 11)]
+    
+    for r in range(1, 11):
+        bb_img = pg.Surface((20 * r, 20 * r))
+        bb_img.set_colorkey((0, 0, 0))
+        pg.draw.circle(bb_img, (255, 0, 0), (10 * r, 10 * r), 10 * r)
+        bb_imgs.append(bb_img)
+            
+    return bb_imgs, bb_accs
 
 
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
@@ -64,6 +77,9 @@ def main():
     bb_img= pg.Surface((20,20))
     bb_img.set_colorkey((0,0,0))
     pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    
+    bb_imgs, bb_accs = init_bb_imgs()
+    bb_img= bb_imgs[0]
     bb_rct=bb_img.get_rect()
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery= random.randint(0, HEIGHT)
@@ -72,6 +88,7 @@ def main():
     bb_img.set_colorkey((0,0,0))
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -79,7 +96,7 @@ def main():
         if kk_rct.colliderect(bb_rct): 
             print("ゲームオーバー")
             gameover(screen)
-            
+
             return
         screen.blit(bg_img, [0, 0]) 
 
@@ -89,18 +106,29 @@ def main():
         for key, mv in DELTA.items(): # first exercice 
             if key_lst[key]:
                 sum_mv[0] += mv[0]  
-                sum_mv[1] += mv[1]  
+                sum_mv[1] += mv[1] 
+         
 
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) 
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy)
+        
+        idx= min(tmr // 500, 9)
+        bb_img = bb_imgs[idx]
+        orig_center = bb_rct.center
+        bb_rct = bb_img.get_rect()
+        bb_rct.center = orig_center
+        avx = vx * bb_accs[idx]
+        avy = vy* bb_accs[idx]
+        bb_rct.move_ip(avx, avy)
         yoko, tate = check_bound(bb_rct)
         if not yoko: 
             vx *= -1
+            bb_rct.move_ip(vx*2, 0)
         if not tate: 
             vy *= -1
+            bb_rct.move_ip(0, vy*2)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
